1ef8af31a1ab3757bace7ea94c52638d5bc41f0e
John Crisostomo <johncrisostomo@protonmail.ch>
2018-06-23 21:30:00 +0800

    Adjusted printer data

--- mocks.js
+++ mocks.js
@@ -1,29 +1,38 @@
 module.exports = {
     printer: {
         lineDataSet: [
-            { date: '6-11', v: 1300 },
-            { date: '6-12', v: 1300 },
-            { date: '6-13', v: 1300 }
+            { date: '6-09', v: 100 },
+            { date: '6-10', v: 52 },
+            { date: '6-11', v: 105 },
+            { date: '6-12', v: 72 },
+            { date: '6-13', v: 90 },
+            { date: '6-14', v: 50 },
+            { date: '6-15', v: 56 },
+            { date: '6-16', v: 24 },
+            { date: '6-17', v: 40 },
+            { date: '6-18', v: 59 },
+            { date: '6-19', v: 80 },
+            { date: '6-20', v: 225 },
+            { date: '6-21', v: 76 },
+            { date: '6-22', v: 24 },
+            { date: '6-23', v: 13 }
         ],
         activityDataSet: [
-            {
-                id: 1,
-                title: 'Printer 1',
-                text: 'Printer used.',
-                type: 'printer'
-            },
-            {
-                id: 2,
-                title: 'Printer 2',
-                text: 'Printer used.',
-                type: 'printer'
-            },
-            {
-                id: 3,
-                title: 'Printer 3',
-                text: 'Printer used.',
-                type: 'printer'
-            }
+            { id: 1, title: '6-09 John', text: 'Printed 100 pages' },
+            { id: 2, title: '6-10 Michael', text: 'Printed 52 pages' },
+            { id: 3, title: '6-11 Leo', text: 'Printed 105 pages' },
+            { id: 4, title: '6-12 Vincent', text: 'Printed 72 pages' },
+            { id: 5, title: '6-13 Andre', text: 'Printed 90 pages' },
+            { id: 6, date: '6-14 Shero', text: 'Printed 50 pages' },
+            { id: 7, date: '6-15 Rachael', text: 'Printed 56 pages' },
+            { id: 8, date: '6-16 Varun', text: 'Printed 24 pages' },
+            { id: 9, date: '6-17 George', text: 'Printed 40 pages' },
+            { id: 10, date: '6-18 Yogesh', text: 'Printed 59 pages' },
+            { id: 11, date: '6-19 Allan', text: 'Printed 80 pages' },
+            { id: 12, date: '6-20 Johnny', text: 'Printed 225' },
+            { id: 13, date: '6-21 Michael', text: 'Printed 76 pages' },
+            { id: 14, date: '6-22 Vincent', text: 'Printed 24 pages' },
+            { id: 15, date: '6-23 John', text: 'Printed 13 pages' }
         ],
         barDataSet: [
             {
