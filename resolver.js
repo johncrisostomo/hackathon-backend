3d5b62da9e3a89a2ef0682f43d6c589c418e971e
John Crisostomo <johncrisostomo@protonmail.ch>
2018-06-23 16:39:27 +0800

    First resolver version

---
+++ resolver.js
@@ -0,0 +1,31 @@
+const { PubSub, withFilter } = require('graphql-subscriptions');
+
+const lineDataSet = [{ pv: 1300 }, { pv: 1300 }, { pv: 1300 }];
+
+const resolvers = {
+    Query: {
+        async getLineDataSet(parentValue, { name }) {
+            return lineDataSet;
+        }
+    },
+    Mutation: {
+        async addToLineDataSet(parentValue) {
+            const pv = { pv: 10000 };
+
+            lineDataSet.append(pv);
+
+            pubsub.publish('lineDataSetUpdated', {
+                pv
+            });
+
+            return pv;
+        }
+    },
+    Subscription: {
+        lineDataSetUpdated: {
+            subscribe: () => pubsub.asyncIterator('lineDataSetUpdated')
+        }
+    }
+};
+
+module.exports = resolvers;
