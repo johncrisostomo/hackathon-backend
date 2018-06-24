1252d40131f93c70bd5eedc893b44d7dff4c585a
John Crisostomo <johncrisostomo@protonmail.ch>
2018-06-24 11:19:54 +0800

    Total printed total 1000

--- resolvers.js
+++ resolvers.js
@@ -26,6 +26,7 @@ const resolvers = {
     Mutation: {
         resetSeed(parentValue) {
             data = _.cloneDeep(mockData());
+
             return 'ok';
         },
         changePrintCardData(parentValue, { value }) {
